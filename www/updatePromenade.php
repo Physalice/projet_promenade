9f1b4fcdbcd88e5544abacf7ebffa6f9d464b8c0
sandra <vuagniauxsandra@gmail.com>
2019-07-15 21:29:38 +0200

    fini formulaire update

--- www/updatePromenade.php
+++ www/updatePromenade.php
@@ -15,7 +15,7 @@ $rando = $database->getRandoById($id);
         <!-- Bootstrap CSS -->
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!-- style CSS -->
-            <link rel="stylesheet" href="newRando.css">
+            <link rel="stylesheet" href="styleRando.css">
         <!----police------>       
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">       
             <title>formulaire-create</title>
